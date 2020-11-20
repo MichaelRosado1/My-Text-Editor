5d602ba91bfb10fd0b595f624ac1fdcaddad0cb9
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-19 23:37:01 -0600

    added files

--- kilo.cpp
+++ kilo.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <unistd.h>
+#include <termios.h>
 
+	
 int main() {
+	//letter typed into terminal
 	char c;
+	//while chars are still being typed in terminal and the letter q hasn't been typed
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
 }
