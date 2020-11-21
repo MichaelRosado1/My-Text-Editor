b8ce312cf8be072b2f4bbb606627b4ff2a4d2ca8
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-20 22:51:47 -0600

    reads terminal input and quits once the char 'q' is entered

--- kilo.cpp
+++ kilo.cpp
@@ -1,25 +1,50 @@
+#include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
 #include <termios.h>
 
+void disableRawMode();
+//use this struct to store the terminals original attributes
+//so when the user is done, we can set the original terminal attributes back  
+struct termios originalTermios;
+
 //function which causes turns off echo in terminal
 void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &originalTermios);
+	atexit(disableRawMode);
+
+
+	//gets the curent terminal attributes
+	tcgetattr(STDIN_FILENO, &originalTermios);
+		
 	// this will store all of the terminal attributes
-	struct termios t;
-	//gets the terminals attributes and stores them in the termios struct
-	tcgetattr(STDIN_FILENO, &t);
-	//turns off the echo feature in the terminal
-	t.c_lflag &=~(ECHO);
-	//sets the new attributes of the terminal
+	struct termios t = originalTermios;
+
+	/*
+		Echo and Icanon are both bit flags
+			->both are represented by binary digits
+			->the | operator goes through the bits and does an OR operation on them.
+			->this essentially will flip both of the bitflags or turn them off
+   */
+	t.c_lflag &= ~(ECHO | ICANON);
+	//TCSAFLUSH specifies when to apply the changes made
+	//	-> makes the change after queued output has been written	
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
 
-
 }
+
+void disableRawMode() {
+	//sets the terminal attributes back to the users original settings 
+	tcsetattr(STDIN_FILENO,TCSAFLUSH, &originalTermios);
+}
+
+
 
 int main() {
 	enableRawMode();
 	//letter typed into terminal
 	char c;
+
 	//while chars are still being typed in terminal and the letter q hasn't been typed
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
