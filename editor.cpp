19a8eb8860ab41a387b2ccf32bb0260cbfc4578a
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-22 16:23:51 -0600

    control q now quits the program

--- editor.cpp
+++ editor.cpp
@@ -2,17 +2,35 @@
 #include <iostream>
 #include <unistd.h>
 #include <termios.h>
+#include <errno.h>
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 void disableRawMode();
+void enableRawMode();
+
+
+/** Data **/
+
 //use this struct to store the terminals original attributes
 //so when the user is done, we can set the original terminal attributes back  
+
 struct termios originalTermios;
+
+/** Terminal **/
+void killPgrm(const char *s) {
+	perror(s);
+	exit(1);
+}
+
 
 //function which causes turns off echo in terminal
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &originalTermios);
+	if (tcgetattr(STDIN_FILENO, &originalTermios) == -1) {
+		killPgrm("tcgetattr");
+	}
 	atexit(disableRawMode);
-		
+
 	//gets the curent terminal attributes
 	tcgetattr(STDIN_FILENO, &originalTermios);
 		
@@ -26,42 +44,64 @@ void enableRawMode() {
 		Echo = printing in terminal
 		ISIG = control c or z(we need to turn this off to prevent the program from ending
 		ICANON = allows us to turn off canonical mode
+		IEXTEN = disables cntrl v
    */
 	
-	t.c_lflag &= ~(ECHO | ISIG| ICANON);
+	t.c_lflag &= ~(ECHO | ISIG| IEXTEN | ICANON);
 	
+
 	// input flag which prevents cntrl s and q from being pressed	
-	t.c_iflag &= ~(IXON);
+	//also fixes cntrl m problem
+	t.c_iflag &= ~(IXON | BRKINT | INPCK |ISTRIP |ICRNL);
+
+	//disables the processing features "\n and \r\n"
+	t.c_oflag &= ~(OPOST);
+
+	t.c_cflag |= (CS8);
+
+	//vmin sets amount of bytes needed  before read() returns anything
+	t.c_cc[VMIN] = 0;
+	//VTIME sets teh amount of time to wait before read() returns anything
+	t.c_cc[VTIME] = 1;
 
 	//TCSAFLUSH specifies when to apply the changes made
 	//	-> makes the change after queued output has been written	
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &t) == -1) {
+		killPgrm("tcsetattr");
+	}
 
 }
+
 
 void disableRawMode() {
 	//sets the terminal attributes back to the users original settings 
-	tcsetattr(STDIN_FILENO,TCSAFLUSH, &originalTermios);
-	
+	if (tcsetattr(STDIN_FILENO,TCSAFLUSH, &originalTermios)) {
+		killPgrm("tcsetattr");
+	}	
 }
 
-
+/** INITIALIZE  **/
 
 int main() {
 	enableRawMode();
 	//letter typed into terminal
-	char c;
+	while (1) {
+		// sets c to char value for 0
+		char c = '\0';
 
-	//while chars are still being typed in terminal and the letter q hasn't been typed
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-		//if the char c is a control(non-printable) character
+		//while chars are still being typed in terminal and the letter q hasn't been typed
+		// EAGIN is the error returned due to time outs(return -1) so ignore it as an error
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+			killPgrm("read");
+		}
+		//if the char c is a control(non-printable) erno character
 		if (iscntrl(c)) {
 			//we only want to print the ascii value of the character
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		} else {
 			//print the ascii value and the letter inputted surrounded by ' 
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
-
+		if (c == CTRL_KEY('q')) break;
 	}
 }
