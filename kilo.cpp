18ca792ccc9a27b73eb745facf939143b0ef2fdc
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-21 20:06:32 -0600

    able to view terminal input and also prevent cntrl options which could terminate the program

--- kilo.cpp
+++ kilo.cpp
@@ -12,21 +12,27 @@ struct termios originalTermios;
 void enableRawMode() {
 	tcgetattr(STDIN_FILENO, &originalTermios);
 	atexit(disableRawMode);
-
-
+		
 	//gets the curent terminal attributes
 	tcgetattr(STDIN_FILENO, &originalTermios);
 		
 	// this will store all of the terminal attributes
 	struct termios t = originalTermios;
-
 	/*
 		Echo and Icanon are both bit flags
 			->both are represented by binary digits
 			->the | operator goes through the bits and does an OR operation on them.
 			->this essentially will flip both of the bitflags or turn them off
+		Echo = printing in terminal
+		ISIG = control c or z(we need to turn this off to prevent the program from ending
+		ICANON = allows us to turn off canonical mode
    */
-	t.c_lflag &= ~(ECHO | ICANON);
+	
+	t.c_lflag &= ~(ECHO | ISIG| ICANON);
+	
+	// input flag which prevents cntrl s and q from being pressed	
+	t.c_iflag &= ~(IXON);
+
 	//TCSAFLUSH specifies when to apply the changes made
 	//	-> makes the change after queued output has been written	
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
@@ -36,6 +42,7 @@ void enableRawMode() {
 void disableRawMode() {
 	//sets the terminal attributes back to the users original settings 
 	tcsetattr(STDIN_FILENO,TCSAFLUSH, &originalTermios);
+	
 }
 
 
@@ -46,6 +53,15 @@ int main() {
 	char c;
 
 	//while chars are still being typed in terminal and the letter q hasn't been typed
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		//if the char c is a control(non-printable) character
+		if (iscntrl(c)) {
+			//we only want to print the ascii value of the character
+			printf("%d\n", c);
+		} else {
+			//print the ascii value and the letter inputted surrounded by ' 
+			printf("%d ('%c')\n", c, c);
+		}
 
+	}
 }
